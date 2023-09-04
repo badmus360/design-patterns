055922a3868b7989fefb93c554209a988c712c3d
badmus360 <olashile.rahman@gmail.com>
2023-09-04 17:48:00 +0100

    first with the Dog class

---
+++ src/main/java/org/example/OopDesignPattern/Dog.java
@@ -0,0 +1,15 @@
+package org.example.OopDesignPattern;
+
+public class Dog extends Animal{
+
+    public void digHole(){
+        System.out.println("Dug a hole");
+    }
+
+    public Dog(){
+        super();
+
+        setSound("Bark");
+    }
+
+}
